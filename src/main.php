0b5024f592ee0456fe3ab25683158ce2a1747669
JhonasRM <jhonas.rodrigues32@gmail.com>
2023-09-25 21:56:33 -0300

    adicionado projeto da aula

---
+++ src/main.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once __DIR__ . "/../vendor/autoload.php";
+
+use App\SystemServices\MonologFactory;
+use Slim\Factory\AppFactory;
+
+$logger  = MonologFactory::getInstance();
+
+$logger -> info("Informação que julgo importante enquanto programo para loggar");
+$logger -> debug("Arquivo main.php rodando...");
+$logger -> error("Vejo que temos um erro aqui...");
+
+$app = AppFactory::create();
+
+$app -> addRoutingMiddleware();
+$app -> add(new BasePathMiddLeware($app));
+$app -> addErrorMiddleware(true, true, true);
+
+$app -> get('/', function (Request $request, Response $response){
+    $response -> getBody() -> write('Hello World!');
+    return $response;
+});
+
+$app -> get('/inserirusuario', function (Request $request, Response $response){
+    $response -> getBody() -> write('Hello World');
+    return $response;
+});
+
+$app -> run();
